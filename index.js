3b82c9bbe9df3803cb0217eea77eee511f49b37d
TSephanya Brun <tsbrun@icloud.com>
2021-04-01 13:53:48 -0400

    commit tree 2004980a2cda9305acf409290da537893dd93592

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+function totalBatteries() {
+    return batteryBatches.reduce((total, count) => total += count, 0)
+}
+
